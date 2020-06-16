b689f1f6f3bf348ae87c7552afa9e8ff5815624b
Gkud <gayatri.kud@gmail.com>
2020-06-16 09:50:54 +0530

    update

--- generateRecipe.py
+++ generateRecipe.py
@@ -1,11 +1,9 @@
 import gpt_2_simple as gpt2
 
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, run_name='run1')
 
 def create_recipe():
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name='run1')
-
-    text = gpt2.generate(sess)
+    text = gpt2.generate(sess, return_as_list=True, length=200)[0]
     text = "\n".join(text.split("."))
     print(text)
-
